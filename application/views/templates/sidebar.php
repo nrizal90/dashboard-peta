0d11bbe9148f12323e7404a1d964f471b5e6dfa4
nrizal90 <nurulrizal90@gmail.com>
2026-08-11 21:47:25 +0700

    Comment out Dashboard Vokasi nav item in sidebar for future use

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -21,12 +21,12 @@
 		</li>
 
 		<!-- Nav Item - Dashboard Vokasi (ringkasan pendataan) -->
-		<li class="nav-item <?= $active === 'pendataan' ? 'active' : '' ?>">
+		<!-- <li class="nav-item <?= $active === 'pendataan' ? 'active' : '' ?>">
 			<a class="nav-link" href="<?= site_url('pendataan') ?>">
 				<i class="fas fa-fw fa-chart-pie"></i>
 				<span>Dashboard Vokasi</span>
 			</a>
-		</li>
+		</li> -->
 
 		<!-- Nav Item - Daftar Pendataan (tabel) -->
 		<li class="nav-item <?= $active === 'daftar-pendataan' ? 'active' : '' ?>">
